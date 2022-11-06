71cffd47040854fd45c6d16584433eb03d645a4a
annaluizah <annah.luizaa@gmail.com>
2022-11-06 20:55:13 -0300

    Variáveis vs. Constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Anna!");
+
+        String meuNome = "Anna Luiza";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // Variáveis vs. Constantes
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        // final deve-se definir a variável sempre em CAIXA ALTA
+        final double VALOR_DE_PI = 3.14;
     }
 }
