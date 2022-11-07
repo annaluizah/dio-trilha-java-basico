3d51380ac9d21615fbba2ea9bcac28a5a379f413
annaluizah <annah.luizaa@gmail.com>
2022-11-06 21:21:28 -0300

    Exemplo tornando variácel positiva ou negativa

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,22 +1,14 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM " + "JAVA";
 
-        System.out.println(nomeCompleto);
+       int numero = 5;
 
-        // Exercício de concatenação
+       numero = - numero;
 
-        String concatenacao = "?";
+       System.out.println(numero);
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+       numero = + numero * -1;
+
+       System.out.println(numero);
     }
 }
